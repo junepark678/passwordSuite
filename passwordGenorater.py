7da7ce442bc300707964da020ad7eec12ad28764
Damon <mdamon.connolly@gmail.com>
2019-08-17 19:28:15 +0100

    Restructured File
    Restructured the file.

--- passwordGenorater.py
+++ passwordGenorater.py
@@ -1,67 +1,74 @@
 import random, pyperclip, argparse
 
 
+def setupArgs():
+    parser = argparse.ArgumentParser()
 
-parser = argparse.ArgumentParser()
+    parser.add_argument('--minAlpha', help='Minimum alphabetical characters to generate', type=int, default=8)
+    parser.add_argument('--maxAlpha', help='Maximum alphabetical characters to generate', type=int, default=8)
+    parser.add_argument('--minNum', help='Minimum numerical characters to generate', type=int, default=3)
+    parser.add_argument('--maxNum', help='Maximum numerical characters to generate', type=int, default=3)
+    parser.add_argument('--minSym', help='Minimum symbolic characters to generate', type=int, default=2)
+    parser.add_argument('--maxSym', help='Maximum symbolic characters to generate', type=int, default=2)
 
-parser.add_argument('--minAlpha', help='Minimum alphabetical characters to generate', type=int, default=8)
-parser.add_argument('--maxAlpha', help='Maximum alphabetical characters to generate', type=int, default=8)
-parser.add_argument('--minNum', help='Minimum numerical characters to generate', type=int, default=3)
-parser.add_argument('--maxNum', help='Maximum numerical characters to generate', type=int, default=3)
-parser.add_argument('--minSym', help='Minimum symbolic characters to generate', type=int, default=2)
-parser.add_argument('--maxSym', help='Maximum symbolic characters to generate', type=int, default=2)
+    parser.add_argument('-p', '--private', help='Hide password in the console window', action='store_true')
 
-parser.add_argument('-p', '--private', help='Hide password in the console window', action='store_true')
-
-args = parser.parse_args()
-
-
-'''Handling error prone input'''
-if args.minAlpha > args.maxAlpha:
-    args.maxAlpha = args.minAlpha + 100
-if args.minNum > args.maxNum:
-    args.maxNum = args.minNum + 100
-if args.minSym > args.maxSym:
-    args.maxSym = args.minSym + 100
+    args = parser.parse_args()
 
 
-password = ""
+    '''Handling error prone input'''
+    if args.minAlpha > args.maxAlpha:
+        args.maxAlpha = args.minAlpha + 100
+    if args.minNum > args.maxNum:
+        args.maxNum = args.minNum + 100
+    if args.minSym > args.maxSym:
+        args.maxSym = args.minSym + 100
 
-symbols = []
-nums = []
+    return args
 
-alphabet = []
-for letter in range(97, 123):
-    alphabet.append(chr(letter))
+def generatePass(args):
+    password = ""
 
-for symbol in range(33, 47):
-    symbols.append(chr(symbol))
+    symbols = []
+    nums = []
+    alphabet = []
 
-for symbol in range(58, 64):
-    symbols.append(chr(symbol))
+    for letter in range(97, 123):
+        alphabet.append(chr(letter))
 
-for num in range(48, 57):
-    nums.append(chr(num))
+    for symbol in range(33, 47):
+        symbols.append(chr(symbol))
+
+    for symbol in range(58, 64):
+        symbols.append(chr(symbol))
+
+    for num in range(48, 57):
+        nums.append(chr(num))
 
 
-for x in range( random.randint(args.minAlpha, args.maxAlpha) ):
-    password = password + random.choice(alphabet)
+    for x in range( random.randint(args.minAlpha, args.maxAlpha) ):
+        password = password + random.choice(alphabet)
+    for x in range(random.randint(args.minNum, args.maxNum)):
+        password = password + random.choice(nums)
+    for x in range(random.randint(args.minSym, args.maxSym)):
+        password = password + random.choice(symbols)
 
-for x in range(random.randint(args.minNum, args.maxNum)):
-    password = password + random.choice(nums)
+    return password
 
-for x in range(random.randint(args.minSym, args.maxSym)):
-    password = password + random.choice(symbols)
 
-if not args.private:
-    print(password)
-else:
-    print('**************')
 
-pyperclip.copy(password)
+if __name__ == '__main__':
 
-print("the password was copied to your clipboard")
+    args = setupArgs()
 
-print("Quick Tip: Use a password manager")
+    password = generatePass(args)
 
-print("These passwords are hard to memorize")
+    if not args.private:
+        print(password)
+    else:
+        print('**************')
+
+    pyperclip.copy(password)
+    print("the password was copied to your clipboard")
+    print("Quick Tip: Use a password manager")
+    print("These passwords are hard to memorize")
